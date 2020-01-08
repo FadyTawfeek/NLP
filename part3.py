488c993e7850c35ba4b16708720f26dba42dd0bc
Wa-wann <degremont.erwann@gmail.com>
2020-01-08 17:27:36 +0100

    use only the similarity algorithm for the ambiguous word instead of doing in on all words in the sentence

--- part3.py
+++ part3.py
@@ -1,7 +1,6 @@
 from pywsd import lesk as l
 from pywsd.similarity import max_similarity as maxsim
 from pywsd import baseline as base
-from pywsd import allwords_wsd as dis
 
 sentence = "Outcomes of AA for special populations."
 ambiguousWord = "AA"
@@ -12,19 +11,9 @@ print(f"adapted lesk : {l.adapted_lesk(sentence, ambiguousWord)}")
 print(f"cosine lesk : {l.cosine_lesk(sentence, ambiguousWord)}")
 print(f"simple lesk : {l.simple_lesk(sentence, ambiguousWord)}")
 
-
-def getTheAmbiguousSynset(disambiguateSentenceArray, ambiguousWord=ambiguousWord):
-    for element in disambiguateSentenceArray:
-        if element[0] == ambiguousWord:
-            return element[2]
-
-
-r1 = dis.disambiguate(sentence, algorithm=maxsim, similarity_option="path", keepLemmas=True)
-r1 = getTheAmbiguousSynset(r1)
-print(f"path-semantic similarity : {r1}")
-
-r2 = dis.disambiguate(sentence, algorithm=maxsim, similarity_option="resnik", keepLemmas=True)
-r2 = getTheAmbiguousSynset(r2)
-print(f"content-semantic similarity : {r2}")
+pathOption = "path"
+resnikOption = "resnik"
+print(f"path-semantic similarity : {maxsim(sentence, ambiguousWord, option=pathOption)}")
+print(f"content-semantic similarity : {maxsim(sentence, ambiguousWord, option=resnikOption)}")
 
 print(f"max lemma count : {base.max_lemma_count(ambiguousWord)}")
